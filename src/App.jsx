7020c342661c2091811c324f14200909b755093e
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-05-17 21:13:55 +0600

    App.jsx file modified and new AppAlt.jsx file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import LayoutComponentOne from "./FunctionalComponentsAndHook/Lesson6/LayoutComponentOne";
-import LayoutComponentTwo from "./FunctionalComponentsAndHook/Lesson6/LayoutComponentTwo";
 
 const App = () => {
 
   return (
     <div className="app">
-      <LayoutComponentOne />
-      <LayoutComponentTwo />
+      <h3>Learning Is Going On.....!</h3>
     </div>
   );
 };
---
+++ src/FunctionalComponentsAndHook/Lesson6/AppAlt.jsx
@@ -0,0 +1,14 @@
+import LayoutComponentOne from "./FunctionalComponentsAndHook/Lesson6/LayoutComponentOne";
+import LayoutComponentTwo from "./FunctionalComponentsAndHook/Lesson6/LayoutComponentTwo";
+
+const AppAlt = () => {
+
+  return (
+    <div className="app">
+      <LayoutComponentOne />
+      <LayoutComponentTwo />
+    </div>
+  );
+};
+
+export default AppAlt;
